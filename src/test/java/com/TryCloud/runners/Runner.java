35f117d38caf1b143be5b48bd49cda6f90a4bcfc
AndrianHrisotimos <MyComputer13>
2022-11-02 15:42:48 +0000

    added FailedRunner

--- src/test/java/com/TryCloud/runners/Runner.java
+++ src/test/java/com/TryCloud/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/TryCloud/step_definitions",
         dryRun = false,
-        tags = "@US-001_CLOUD-1343"
+        tags = ""
 
    )
 public class Runner {
